62601b58580fa6d67ca9875ff471e3d482fa5a5f
na-yang <nyang38@hotmail.com>
2023-08-14 11:25:43 -0400

    commented on my code

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -3,7 +3,13 @@ nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "income", "res_1980", "res_2002", "age_bir")
 
 library(tidyverse)
+
+#this doesn't work because we donn't all have the same directory
 setwd("~/Documents/Teaching/Emory/epi590r-in-class/data/raw/")
+
+#corrected file path?
+setwd("~/Users/nalya/OneDrive/Documents/RSPH MPH/Courses/2023 FA/EPI 590R/epi590r-in-class/data/raw")
+
 nlsy <- read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
