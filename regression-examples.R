ad8e361e3b286372405b70ea111fdbde92bff4b5
na-yang <nyang38@hotmail.com>
2023-08-15 08:55:43 -0400

    regression examples, fixing code

--- regression-examples.R
+++ regression-examples.R
@@ -96,3 +96,57 @@ tbl_int <- tbl_regression(
 tbl_merge(list(tbl_no_int, tbl_int),
 					tab_spanner = c("**Model 1**", "**Model 2**"))
 
+
+###you try exercises:
+
+# 3.)
+tbl_uvregression(
+	nlsy,
+	x = sex_cat,
+	include = c(nsibs, starts_with("sleep"), income),
+	method = lm)
+#or do it this way:
+tbl_uvregression(
+	nlsy,
+	y = nsibs,
+	include = sex_cat,
+	method = lm)
+# do for each variable it is asking for, then do a table merge
+
+
+# 4.)
+poisson_model <- glm(nsibs ~ eyesight_cat + sex_cat + income,
+											data = nlsy, family = poisson())
+#table:
+tbl_regression(
+	poisson_model,
+	exponentiate = TRUE,
+	label = list(
+		sex_cat ~ "Sex",
+		eyesight_cat ~ "Eyesight",
+		income ~ "Income"
+	))
+
+# 5.)
+tbl_uvregression(
+	nlsy,
+	y = glasses,
+	include = c(sex_cat, eyesight_cat),
+	method = glm,
+	method.args = list(family = binomial(link = "log")),
+	exponentiate = TRUE)
+
+# 6.)
+eyes_binomial <- glm(glasses ~ eyesight_cat + sex_cat,
+										 family = binomial(link = "log"), data = nlsy)
+eyes_poisson <- glm(glasses ~ eyesight_cat + sex_cat,
+										 family = poisson(link = "log"), data = nlsy)
+tbl_eyes_binomial <- tbl_regression(eyes_binomial,
+																		exponentiate = TRUE,
+																		tidy_fun = partial(tidy_robust, vcov= "HC1"))
+tbl_eyes_poisson <- tbl_regression(eyes_poisson,
+																		exponentiate = TRUE,
+																		tidy_fun = partial(tidy_robust, vcov= "HC1"))
+#then merge tables
+tbl_merge(list(tbl_eyes_binomial, tbl_eyes_poisson),
+					tab_spanner = c("**binomial**", "**poisson**"))
